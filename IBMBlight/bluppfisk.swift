abc1fcd8e96577a11718cbbdbcaa89fc05e242b5
agoofih <daniel.barwen@gmail.com>
2018-03-22 14:48:53 +0100

    test dev change

---
+++ IBMBlight/bluppfisk.swift
@@ -0,0 +1,13 @@
+//
+//  bluppfisk.swift
+//  IBMBlight
+//
+//  Created by Daniel T. Barwén on 2018-03-22.
+//  Copyright © 2018 Daniel T. Barwén. All rights reserved.
+//
+
+import UIKit
+
+class bluppfisk: NSObject {
+
+}
