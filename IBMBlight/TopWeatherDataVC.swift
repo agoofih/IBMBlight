23da32515cb40a04000a020834b16ccc6af18986
agoofih <daniel.barwen@gmail.com>
2018-04-19 14:28:16 +0200

    added graphics that helps the user

--- IBMBlight/TopWeatherDataVC.swift
+++ IBMBlight/TopWeatherDataVC.swift
@@ -99,104 +99,104 @@ class TopWeatherDataVC: UIViewController, CLLocationManagerDelegate {
                                     let t = UserDefaults.standard.value(forKey: "weatherWindDirection") as! Int
                                     switch t {
                                     case 0 ... 11:
-                                        self.windDirection.text = "Bearing : N"
+                                        self.windDirection.text = ": N"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 8))
                                         }, completion: nil)
                                     case 11 ... 34:
-                                        self.windDirection.text = "Bearing : NNE"
+                                        self.windDirection.text = ": NNE"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 9))
                                         }, completion: nil)
                                     case 34 ... 56:
-                                        self.windDirection.text = "Bearing : NE"
+                                        self.windDirection.text = ": NE"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 10))
                                         }, completion: nil)
                                     case 56 ... 79:
-                                        self.windDirection.text = "Bearing : ENE"
+                                        self.windDirection.text = ": ENE"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 11))
                                         }, completion: nil)
                                     case 79 ... 101:
-                                        self.windDirection.text = "Bearing : E"
+                                        self.windDirection.text = ": E"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 12))
                                         }, completion: nil)
                                     case 101 ... 124:
-                                        self.windDirection.text = "Bearing : ESE"
+                                        self.windDirection.text = ": ESE"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 13))
                                         }, completion: nil)
                                     case 124 ... 146:
-                                        self.windDirection.text = "Bearing : SE"
+                                        self.windDirection.text = ": SE"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 14))
                                         }, completion: nil)
                                     case 146 ... 169:
-                                        self.windDirection.text = "Bearing : SSE"
+                                        self.windDirection.text = ": SSE"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 15))
                                         }, completion: nil)
                                     case 169 ... 191:
-                                        self.windDirection.text = "Bearing : S"
+                                        self.windDirection.text = ": S"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 16))
                                         }, completion: nil)
                                         
                                     case 191 ... 214:
-                                        self.windDirection.text = "Bearing : SSW"
+                                        self.windDirection.text = ": SSW"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 1))
                                         }, completion: nil)
                                     case 214 ... 236:
-                                        self.windDirection.text = "Bearing : SW"
+                                        self.windDirection.text = ": SW"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 2))
                                         }, completion: nil)
                                     case 236 ... 259:
-                                        self.windDirection.text = "Bearing : WSW"
+                                        self.windDirection.text = ": WSW"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 3))
                                         }, completion: nil)
                                     case 259 ... 281:
-                                        self.windDirection.text = "Bearing : W"
+                                        self.windDirection.text = ": W"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 4))
                                         }, completion: nil)
                                     case 281 ... 304:
-                                        self.windDirection.text = "Bearing : WNW"
+                                        self.windDirection.text = ": WNW"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 5))
                                         }, completion: nil)
                                     case 304 ... 326:
-                                        self.windDirection.text = "Bearing : NW"
+                                        self.windDirection.text = ": NW"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 6))
                                         }, completion: nil)
                                     case 326 ... 349:
-                                        self.windDirection.text = "Bearing : NNW"
+                                        self.windDirection.text = ": NNW"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 7))
                                         }, completion: nil)
                                     case 349 ... 360:
-                                        self.windDirection.text = "Bearing : N"
+                                        self.windDirection.text = ": N"
                                         self.windDirectionArrow.transform = CGAffineTransform.identity
                                         UIView.animate(withDuration: 1, animations: {
                                             self.windDirectionArrow.transform = CGAffineTransform(rotationAngle: CGFloat(self.piSixteensths * 8))
