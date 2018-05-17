25fbbd70c178a1f0b1be89071d705727b2c1be74
agoofih <daniel.barwen@gmail.com>
2018-05-17 10:23:31 +0200

    removed unwanted / unused files in main folder

--- IBMBlight/NewsViewController.swift
+++ IBMBlight/NewsViewController.swift
@@ -1,42 +1,42 @@
+////
+////  NewsViewController.swift
+////  IBMBlight
+////
+////  Created by Daniel T. Barwén on 2018-04-03.
+////  Copyright © 2018 Daniel T. Barwén. All rights reserved.
+////
 //
-//  NewsViewController.swift
-//  IBMBlight
+//import UIKit
 //
-//  Created by Daniel T. Barwén on 2018-04-03.
-//  Copyright © 2018 Daniel T. Barwén. All rights reserved.
+//class NewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 //
-
-import UIKit
-
-class NewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    @IBOutlet weak var newsTableView: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        newsTableView.register(NewsTableViewCell.self, forCellReuseIdentifier: "newsCell")
-    }
-    
-    var tempNews = ["1. BREAKING NEWS! Don't know", "2. Well there has to be something new... No?", "3. Some hackers did something bad. No shit..!", "4. The world ENDS! Bad day for us all. Free icecream!", "5. Something more something less, donno.., awdwada dadadad a d ada da dadad adad adada ad a dadada d adadad adada ", "6. Ohh well aliens won, new bosses.. Most other people dead.. maybe it's MY TIME TO SHINE!! Take the good with the bad"]
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tempNews.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath) as! NewsTableViewCell
-        
-        tableView.separatorStyle = .none
-        if cell.newsLabel != nil {
-             cell.newsLabel.text = tempNews[indexPath.row]
-        }
-
-        return cell
-        
-    }
-
-}
+//    @IBOutlet weak var newsTableView: UITableView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//        newsTableView.register(NewsTableViewCell.self, forCellReuseIdentifier: "newsCell")
+//    }
+//
+//    var tempNews = ["1. BREAKING NEWS! Don't know", "2. Well there has to be something new... No?", "3. Some hackers did something bad. No shit..!", "4. The world ENDS! Bad day for us all. Free icecream!", "5. Something more something less, donno.., awdwada dadadad a d ada da dadad adad adada ad a dadada d adadad adada ", "6. Ohh well aliens won, new bosses.. Most other people dead.. maybe it's MY TIME TO SHINE!! Take the good with the bad"]
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return tempNews.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath) as! NewsTableViewCell
+//
+//        tableView.separatorStyle = .none
+//        if cell.newsLabel != nil {
+//             cell.newsLabel.text = tempNews[indexPath.row]
+//        }
+//
+//        return cell
+//
+//    }
+//
+//}
